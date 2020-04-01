c78aa3126e297219702f3786bf543286fc68012d
Mark Guevarra <markguevarra24@gmail.com>
2020-04-01 13:42:37 +0800

    Basic PHP

---
+++ basic.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>PHP The Fundamentals Page</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello World! from PHP Echo.";
+        ?>
+    </body>
+</html>
